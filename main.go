47d84df2e9558301a6004f0cfd0f12302e791095
seokjoong <happychrono@gmail.com>
2015-11-11 13:05:53 +0900

    gofmt

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
-import  (
+import (
+	"container/list"
 	"github.com/Syfaro/telegram-bot-api"
 	"log"
-	"container/list"
 	"time"
 )
-type ReceivedMessage struct{
-	message tgbotapi.MessageConfig
+
+type ReceivedMessage struct {
+	message         tgbotapi.MessageConfig
 	replyChatIdList *list.List
 }
+
 var (
 	MessageList list.List
 	MessageChan = make(chan tgbotapi.Message)
 )
-func AddNewMessage (message tgbotapi.MessageConfig) {
+
+func AddNewMessage(message tgbotapi.MessageConfig) {
 	MessageList.PushBack(ReceivedMessage{message, new(list.List)})
 }
-func (r ReceivedMessage)IsSentMessage(ChatId int) bool{
+func (r ReceivedMessage) IsSentMessage(ChatId int) bool {
 	x := r.replyChatIdList
 	for e := x.Front(); e != nil; e = e.Next() {
 		b := e.Value.(int)
@@ -26,10 +29,10 @@ func (r ReceivedMessage)IsSentMessage(ChatId int) bool{
 		}
 	}
 	return false
-	
+
 }
 
-func GetReplyMessage(ChatId int) tgbotapi.MessageConfig{
+func GetReplyMessage(ChatId int) tgbotapi.MessageConfig {
 	for e := MessageList.Front(); e != nil; e = e.Next() {
 		b := e.Value.(ReceivedMessage)
 		if b.message.ChatID != ChatId && b.IsSentMessage(ChatId) != true {
@@ -44,40 +47,39 @@ func GetReplyMessage(ChatId int) tgbotapi.MessageConfig{
 }
 func SendReplyMessage(bot *tgbotapi.BotAPI) {
 	for {
-			updatemsg := <-MessageChan
-			msg := tgbotapi.NewMessage(updatemsg.Chat.ID, updatemsg.From.LastName + updatemsg.From.FirstName + ": " + updatemsg.Text)
-			AddNewMessage(msg)
-			replyMsg := GetReplyMessage(updatemsg.Chat.ID)
-			if replyMsg.ChatID != -1 {
-				bot.SendMessage(replyMsg)
-			}
-			time.Sleep(100*time.Millisecond)
+		updatemsg := <-MessageChan
+		msg := tgbotapi.NewMessage(updatemsg.Chat.ID, updatemsg.From.LastName+updatemsg.From.FirstName+": "+updatemsg.Text)
+		AddNewMessage(msg)
+		replyMsg := GetReplyMessage(updatemsg.Chat.ID)
+		if replyMsg.ChatID != -1 {
+			bot.SendMessage(replyMsg)
 		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 func main() {
 
-    bot, err := tgbotapi.NewBotAPI("YOUR_BOT_API_TOKEN")
-    if err != nil {
-        log.Panic(err)
-    }
+	bot, err := tgbotapi.NewBotAPI("YOUR_BOT_API_TOKEN")
+	if err != nil {
+		log.Panic(err)
+	}
 
-    bot.Debug = true
+	bot.Debug = true
 
-    log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-    err = bot.UpdatesChan(u)
-    if err != nil {
-        log.Panic(err)
-    }
+	err = bot.UpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
 	go SendReplyMessage(bot)
 
-	
-    for update := range bot.Updates {
-        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		MessageChan<-update.Message
-		time.Sleep(100*time.Millisecond)
-    }
+	for update := range bot.Updates {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		MessageChan <- update.Message
+		time.Sleep(100 * time.Millisecond)
+	}
 }
